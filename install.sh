3cd794c0e13728345aa8f411c1534a19b1073586
TripleHat <68332137+TripleHat@users.noreply.github.com>
2021-07-17 16:08:26 +0300

    Update install.sh
    For Ani3

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 cd; cd $PREFIX/etc
+if [[ -e motd ]]; then
+rm motd
+fi
 rm bash.bashrc
-wget https://raw.githubusercontent.com/TripleHat/THT-SHELL/master/bash.bashrc
+wget -q "https://raw.githubusercontent.com/TripleHat/THT-SHELL/master/bash.bashrc"
 clear; echo "RESTART YOUR TERMUX"
